14808542484dfdaef33d52ef408fccd5a8616b94
Angel Valentin <angelgvalentin7@gmail.com>
2022-05-23 15:04:59 -0500

    Added modal and error handling

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useState} from "react";
+import Form from "./components/Form";
+import Users from "./components/Users";
 
+// const usersList = [
+//     {
+//         name: "test",
+//         age: "1",
+//     },
+//     {
+//         name: "Test2",
+//         age: "2",
+//     },
+// ];
 
 function App() {
-  return (
-    <div>
+    const [users, setUsers] = useState([]);
 
-    </div>
-  );
+    // const addUser = (newUser) => {
+    //     setUsers((prevUsers) => {
+    //         return [newUser, ...prevUsers];
+    //     });
+    const addUser = (uName, uAge) => {
+        setUsers((prevUsersList) => {
+            return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}];
+        });
+    };
+
+    return (
+        <div>
+            <Form onNewUser={addUser} />
+            <Users data={users} />
+        </div>
+    );
 }
 
 export default App;
---
+++ 01-starting-project/src/components/UI/ErrorModal.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "./Button";
+import Card from "./Card";
+import style from "./ErrorModal.module.css";
+
+const ErrorModal = (props) => {
+    const closeModal = () => {
+        props.onModalClose();
+    };
+
+    return (
+        <div>
+            <div className={style.backdrop}></div>
+            <Card className={style.modal}>
+                <header className={style.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={style.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={style.actions}>
+                    <Button onClick={closeModal}>Okay</Button>
+                </footer>
+            </Card>
+        </div>
+    );
+};
+
+export default ErrorModal;
